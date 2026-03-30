66e016c99861c40f4b0a7a29a3c98b555e836db3
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-03-31 00:08:35 +0900

    fix data read index

--- homepage/data/data_read.py
+++ homepage/data/data_read.py
@@ -15,20 +15,21 @@ def read_option(sheet_id, row, end):
 
 def get_program_data(program_name):
     program_data = {
-            "name": program_name,
+            "program_name": program_name,
             "url": str(program_df[program_name][0]),
+            "image_name": str(program_df[program_name][1]),
             "outline": " ".join(program_df[program_name][3].split()),
             "period": " ".join(program_df[program_name][4].split()),
             "schedule": " ".join(str(program_df[program_name][5]).split()),
             "cost": " ".join(str(program_df[program_name][6]).split()),
             "application_grade": " ".join(str(program_df[program_name][7]).split()),
             "contact": " ".join(str(program_df[program_name][8]).split()),
-            "ID": str(program_df[program_name][1])
         }
     return program_data
 
 judge_data = {}
 option_data = {}
+option_count = {}
 
 for i in range(judge_df.shape[0] - 1):
     programs_list = judge_df.iloc[i+1, 2:].dropna()
@@ -54,18 +55,18 @@ for i in range(judge_df.shape[0] - 1):
 
 judge_df = pd.read_excel("updated_judge.xlsx",sheet_name=1,header=None)
 
-language = ['L1','L2','L3','L4','L5']
-period = ['P1','P2','P3','P4']
-k = -1
-p = -1
+option_count["P"] = i + 1
+
+l = 0
+p = 0
 for i in range(judge_df.shape[0] - 1):
     programs_list = judge_df.iloc[i + 1, 3:].dropna()
     p += 1
     if i % 4 == 0:
-        k += 1
-        p = 0
+        l += 1
+        p = 1
 
-    query = f"H2{language[k % 5]}{period[p % 4]}"
+    query = f"H2L{l}P{p}"
     program_entries = []
     option = ""
 
@@ -81,19 +82,20 @@ for i in range(judge_df.shape[0] - 1):
     if option:
         option_data[query] = option
 
+option_count["L"] = l + 1
+
 judge_df = pd.read_excel("updated_judge.xlsx",sheet_name=2,header=None)
 season = ['S1','S2']
-period = ['P1','P2','P3','P4']
-k = -1
-p = -1
+s = 0
+p = 0
 for i in range(judge_df.shape[0] - 1):
     programs_list = judge_df.iloc[i + 1, 3:].dropna()
     p += 1
     if i % 4 == 0:
-        k += 1
-        p = 0
+        s += 1
+        p = 1
 
-    query = f"H3{season[(k)%2]}{period[p%4]}"
+    query = f"H3S{s}P{p}"
     program_entries = []
     option = ""
 
@@ -109,20 +111,22 @@ for i in range(judge_df.shape[0] - 1):
     if option:
         option_data[query] = option
 
+option_count["S"] = s + 1
 
 judge_df = pd.read_excel("updated_judge.xlsx",sheet_name=3,header=None)
 purpose = ['PP1','PP2','PP3','PP4','PP5','PP6','PP7','PP8','PP9','PP10','PP11','PP12', 'PP13', 'PP14', 'PP15', 'PP16']
 period = ['P1','P2','P3','P4']
-k = -1
-p = -1
+
+pp = 1
+p = 1
 for i in range(judge_df.shape[0]-1):
     programs_list = judge_df.iloc[i+1,3:].dropna()
     p += 1
-    if i %4 == 0:
-        k+=1
-        p = 0
+    if i % 4 == 0:
+        pp += 1
+        p = 1
 
-    query = f"H4{purpose[(k)%16]}{period[p%4]}"
+    query = f"H4PP{pp}P{p}"
     program_entries = []
     option = ""
 
@@ -138,21 +142,22 @@ for i in range(judge_df.shape[0]-1):
     if option:
         option_data[query] = option
 
-
+option_count["PP"] = pp + 1
 
 judge_df = pd.read_excel("updated_judge.xlsx",sheet_name=4,header=None)
 style = ['Style1','Style2','Style3','Style4','Style5','Style6','Style7',]
 period = ['P1','P2','P3','P4']
-k = -1
-p = -1
+
+style = 1
+p = 1
 for i in range(judge_df.shape[0]-1):
     programs_list = judge_df.iloc[i+1,3:].dropna()
     p += 1
-    if i %4 == 0:
-        k+=1
-        p = 0
+    if i % 4 == 0:
+        style += 1
+        p = 1
 
-    query = f"H5{style[(k)%7]}{period[p%4]}"
+    query = f"H5Style{style}P{p}"
     program_entries = []
     option = ""
 
@@ -167,6 +172,8 @@ for i in range(judge_df.shape[0]-1):
     judge_data[query] = program_entries
     if option:
         option_data[query] = option
+
+option_count["Style"] = style + 1
 
 judge_df = pd.read_excel("updated_judge.xlsx",sheet_name=5,header=None)
 
@@ -191,9 +198,14 @@ for i in range(judge_df.shape[0] - 1):
     if option:
         option_data[query] = option
 
+option_count["E"] = i + 1
+
 # Optional: Save to JSON file with pretty formatting
 with open("judge_data.json", "w", encoding="utf-8") as f:
     json.dump(judge_data, f, ensure_ascii=False, indent=4)
 
 with open("option_data.json", "w", encoding="utf-8") as f:
     json.dump(option_data, f, ensure_ascii=False, indent=4)
+
+with open("option_count.json", "w", encoding="utf-8") as f:
+    json.dump(option_count, f, ensure_ascii=False, indent=4)
